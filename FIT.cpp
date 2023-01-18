1be7ca4566179af0402f567fe927183bfac8a4c0
Ash <89018395+ash413@users.noreply.github.com>
2023-01-18 21:10:21 +0530

    Add files via upload

---
+++ FIT.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        cout<<2*5*x<<endl;
+    }
+	// your code goes here
+	return 0;
+}
